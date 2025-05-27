29c8fca3144a55a096298d204308ac9a7056dca9
CharlesAragao <charless.dev17@gmail.com>
2025-05-27 16:14:24 -0300

    feat/app.js

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 
+import express from 'express';
 import router from "./routes/userRoutes.js";
+import logger from "./middleware/logger.js"
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(logger);
+
+// Rotas
+app.use('/api/users', userRoutes);
+
+// Servidor
+const host = "localhost";
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://${host}:${port}`);
+});
